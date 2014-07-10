abd4b799da743118d3667d7ac499f56edad8969f
Dennis D. Kirkpatrick <ddkirkpat@gmail.com>
2014-07-10 09:30:35 -0600

    Recommit of rspec tests after repository restructure

--- FizzBuzzFun/spec/fizzbuzzfun_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe 'FizzBuzzFun' do
-  context 'knows that a number is divisible by' do
-    it '3' do
-      expect(is_divisible_by_three?(3)).to be_true
-    end
-  end
-end
---
+++ spec/fizzbuzzfun_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe 'FizzBuzzFun' do
+  context 'knows that a number is divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(3)).to be_truthy
+    end
+  end
+  context 'knows that a number is not divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(1)).to be_falsey
+    end
+  end
+end
