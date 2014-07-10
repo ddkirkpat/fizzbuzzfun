3d8feecd34ae16c6e7f7e76bc80e2eebb6b9f438
Dennis D. Kirkpatrick <ddkirkpat@gmail.com>
2014-07-10 09:47:19 -0600

    Added tests and code for fifteen

--- lib/fizzbuzzfun.rb
+++ lib/fizzbuzzfun.rb
@@ -6,6 +6,10 @@ def is_divisible_by_five?(number)
   is_divisible_by(number, 5)
 end
 
+def is_divisible_by_fifteen?(number)
+  is_divisible_by(number, 15)
+end
+
 def is_divisible_by(number, divisor)
   number % divisor == 0
 end
--- spec/fizzbuzzfun_spec.rb
+++ spec/fizzbuzzfun_spec.rb
@@ -6,7 +6,10 @@ describe 'FizzBuzzFun' do
       expect(is_divisible_by_three?(3)).to be_truthy
     end
     it '5' do
-          expect(is_divisible_by_five?(5)).to be_truthy
+      expect(is_divisible_by_five?(5)).to be_truthy
+    end
+    it '15' do
+      expect(is_divisible_by_fifteen?(15)).to be_truthy
     end
   end
   context 'knows that a number is not divisible by' do
@@ -15,6 +18,9 @@ describe 'FizzBuzzFun' do
     end
     it '5' do
       expect(is_divisible_by_five?(1)).to be_falsey
+    end
+    it '15' do
+      expect(is_divisible_by_fifteen?(1)).to be_falsey
     end
   end
 end
