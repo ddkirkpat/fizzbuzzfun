82663ea92febd2468710a14e21de1d8bfc4cb4f0
Dennis D. Kirkpatrick <ddkirkpat@gmail.com>
2014-07-10 09:43:32 -0600

    Added divisible by 5 tests and then code

--- lib/fizzbuzzfun.rb
+++ lib/fizzbuzzfun.rb
@@ -2,6 +2,10 @@ def is_divisible_by_three?(number)
   is_divisible_by(number, 3)
 end
 
+def is_divisible_by_five?(number)
+  is_divisible_by(number, 5)
+end
+
 def is_divisible_by(number, divisor)
   number % divisor == 0
 end
--- spec/fizzbuzzfun_spec.rb
+++ spec/fizzbuzzfun_spec.rb
@@ -5,10 +5,16 @@ describe 'FizzBuzzFun' do
     it '3' do
       expect(is_divisible_by_three?(3)).to be_truthy
     end
+    it '5' do
+          expect(is_divisible_by_five?(5)).to be_truthy
+    end
   end
   context 'knows that a number is not divisible by' do
     it '3' do
       expect(is_divisible_by_three?(1)).to be_falsey
+    end
+    it '5' do
+      expect(is_divisible_by_five?(1)).to be_falsey
     end
   end
 end
