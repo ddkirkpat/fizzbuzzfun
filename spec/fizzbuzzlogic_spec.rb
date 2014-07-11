54d6f56099c40559ba5ea68706e64ed7362d4b85
Dennis D. Kirkpatrick <ddkirkpat@gmail.com>
2014-07-10 20:37:20 -0600

    Refactored rspec tests for testing against class:methods for fizzbuzzlogic

--- spec/fizzbuzzlogic_spec.rb
+++ spec/fizzbuzzlogic_spec.rb
@@ -1,40 +1,71 @@
 require "spec_helper"
 
 describe 'FizzBuzzLogic' do
-  context 'knows that a number is divisible by' do
-    it '3' do
-      expect(is_divisible_by_three?(3)).to be_truthy
+  
+  describe 'is_divisible_by_three?' do
+    context 'knows that a number is divisible by' do
+      it '3' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.is_divisible_by_three?(3)).to be_truthy
+      end
     end
-    it '5' do
-      expect(is_divisible_by_five?(5)).to be_truthy
-    end
-    it '15' do
-      expect(is_divisible_by_fifteen?(15)).to be_truthy
-    end
-  end
-  context 'knows that a number is not divisible by' do
-    it '3' do
-      expect(is_divisible_by_three?(1)).to be_falsey
-    end
-    it '5' do
-      expect(is_divisible_by_five?(1)).to be_falsey
-    end
-    it '15' do
-      expect(is_divisible_by_fifteen?(1)).to be_falsey
+    context 'knows that a number is not divisible by' do
+      it '3' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.is_divisible_by_three?(1)).to be_falsey
+      end
     end
   end
-  context 'while playing the game, it returns' do
-    it 'the number' do
-      expect(fizzbuzzlogic(1)).to eq 1
+
+  describe 'is_divisible_by_five?' do
+    context 'knows that a number is divisible by' do
+      it '5' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.is_divisible_by_five?(5)).to be_truthy
+      end
     end
-    it 'Fizz' do
-      expect(fizzbuzzlogic(3)).to eq "Fizz"
-    end
-    it 'Buzz' do
-      expect(fizzbuzzlogic(5)).to eq "Buzz"
-    end
-    it 'FizzBuzz' do
-      expect(fizzbuzzlogic(15)).to eq "FizzBuzz"
+    context 'knows that a number is not divisible by' do
+      it '5' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.is_divisible_by_five?(1)).to be_falsey
+      end
     end
   end
+
+  describe 'is_divisible_by_fifteen?' do
+    context 'knows that a number is divisible by' do
+      it '15' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.is_divisible_by_five?(15)).to be_truthy
+      end
+    end
+    context 'knows that a number is not divisible by' do
+      it '15' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.is_divisible_by_fifteen?(1)).to be_falsey
+      end
+    end
+  end
+
+  describe 'fizzbuzzprint' do
+    context 'while playing the game, it returns' do
+      it 'the number' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.fizzbuzzprint(1)).to eq 1
+      end
+      it 'Fizz' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.fizzbuzzprint(3)).to eq "Fizz"
+      end
+      it 'Buzz' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.fizzbuzzprint(5)).to eq "Buzz"
+      end
+      it 'FizzBuzz' do
+        fizzbuzzlogic = FizzBuzzLogic.new
+        expect(fizzbuzzlogic.fizzbuzzprint(15)).to eq "FizzBuzz"
+      end
+    end
+  end
+
 end
