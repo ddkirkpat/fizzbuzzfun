c7eecaf3c4d33f0c2189dcc8c89349848a87943b
Dennis D. Kirkpatrick <ddkirkpat@gmail.com>
2014-07-11 15:12:45 -0600

    Refactored to only use methods as a Stage 1 goal

--- spec/fizzbuzzlogic_spec.rb
+++ spec/fizzbuzzlogic_spec.rb
@@ -1,18 +1,16 @@
 require "spec_helper"
 
 describe 'FizzBuzzLogic' do
-  
+
   describe 'is_divisible_by_three?' do
     context 'knows that a number is divisible by' do
       it '3' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.is_divisible_by_three?(3)).to be_truthy
+        expect(is_divisible_by_three?(3)).to be_truthy
       end
     end
     context 'knows that a number is not divisible by' do
       it '3' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.is_divisible_by_three?(1)).to be_falsey
+        expect(is_divisible_by_three?(1)).to be_falsey
       end
     end
   end
@@ -20,29 +18,25 @@ describe 'FizzBuzzLogic' do
   describe 'is_divisible_by_five?' do
     context 'knows that a number is divisible by' do
       it '5' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.is_divisible_by_five?(5)).to be_truthy
+        expect(is_divisible_by_five?(5)).to be_truthy
       end
     end
     context 'knows that a number is not divisible by' do
       it '5' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.is_divisible_by_five?(1)).to be_falsey
-      end
+        expect(is_divisible_by_five?(1)).to be_falsey
+      end 
     end
   end
 
   describe 'is_divisible_by_fifteen?' do
     context 'knows that a number is divisible by' do
       it '15' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.is_divisible_by_five?(15)).to be_truthy
+        expect(is_divisible_by_five?(15)).to be_truthy
       end
     end
     context 'knows that a number is not divisible by' do
       it '15' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.is_divisible_by_fifteen?(1)).to be_falsey
+        expect(is_divisible_by_fifteen?(1)).to be_falsey
       end
     end
   end
@@ -50,20 +44,16 @@ describe 'FizzBuzzLogic' do
   describe 'fizzbuzzprint' do
     context 'while playing the game, it returns' do
       it 'the number' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.fizzbuzzprint(1)).to eq 1
+        expect(fizzbuzzprint(1)).to eq 1
       end
       it 'Fizz' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.fizzbuzzprint(3)).to eq "Fizz"
+        expect(fizzbuzzprint(3)).to eq "Fizz"
       end
       it 'Buzz' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.fizzbuzzprint(5)).to eq "Buzz"
+        expect(fizzbuzzprint(5)).to eq "Buzz"
       end
       it 'FizzBuzz' do
-        fizzbuzzlogic = FizzBuzzLogic.new
-        expect(fizzbuzzlogic.fizzbuzzprint(15)).to eq "FizzBuzz"
+        expect(fizzbuzzprint(15)).to eq "FizzBuzz"
       end
     end
   end
